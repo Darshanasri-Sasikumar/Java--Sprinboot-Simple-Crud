79bb260ad46a6a30e6c4af2b8d013e2444660890
Darshana <darshana@cloudrevelinnovation.com>
2025-12-24 10:53:31 +0530

    completed Crud

--- src/main/java/com/example/demo/controller/bookController.java
+++ src/main/java/com/example/demo/controller/bookController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.Model.Book;
 import com.example.demo.repo.bookrepo;
+import java.util.List;
 
 @RestController
 @RequestMapping("/book")
@@ -18,8 +19,38 @@ public class bookController {
         return bookRepo.save(book);
     }
 
-    public Book update(@RequestBody Book book){
-        return bookRepo.up
+    @GetMapping
+    public List<Book> listBooks() {
+        return bookRepo.findAll();
+    }
+
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable Long id, @RequestBody Book book) {
+
+        Book existingBook = bookRepo.findById(id).orElse(null);
+
+        if (existingBook != null) {
+            existingBook.setTitle(book.getTitle());
+            existingBook.setAuthor(book.getAuthor());
+            return bookRepo.save(existingBook);
+        }
+
+        return null;
+    }
+    // @DeleteMapping("/{id}")
+    // public void Delete(@PathVariable Long id) {
+    // bookRepo.findById(id);
+    // }
+
+    // or we can do by providing string to delete
+
+    @DeleteMapping("/{id}")
+    public String deleteBook(@PathVariable Long id) {
+        if (bookRepo.existsById(id)) {
+            bookRepo.deleteById(id);
+            return "Book deleted successfully";
+        }
+        return "Book not found";
     }
 
 }
